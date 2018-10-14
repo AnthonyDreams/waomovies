2ce98d3ec79e22a3cb6cc9593b10a56cccae0e27
Anthony Taveras <theanthony2d@gmail.com>
2018-10-13 20:38:54 -0400

    make it better

--- Wao/settings.py
+++ Wao/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j$z5qv+cug3pd8p6#jnbj+mdn$0x#fonrlk#=&f*1f0_e)=&yt'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['wmoviestest.herokuapp.com']
 
@@ -162,7 +162,13 @@ EMAIL_HOST_USER = 'theanthony2d@gmail.com'
 DEFAULT_FROM_EMAIL = 'testing@testing.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
+ADMINS = (
+('Anthony2D', 'theanthony2d@gmail.com')
 
+
+	)
+
+MANAGERS = ADMINS
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
