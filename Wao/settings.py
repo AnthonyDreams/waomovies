a8bf9adeee048b42883c5d648a12701bcdf5de99
Anthony Taveras <theanthony2d@gmail.com>
2018-10-13 21:43:16 -0400

    make it better

--- Wao/settings.py
+++ Wao/settings.py
@@ -158,7 +158,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'theanthony2d@gmail.com'
+EMAIL_HOST_USER = 'waosoporte@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 import dj_database_url
