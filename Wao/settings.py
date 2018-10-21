c470e590d4441b298926ac2fb974e9c953ddf3e5
Anthony Taveras <theanthony2d@gmail.com>
2018-10-20 22:58:31 -0400

    statictos3

--- Wao/settings.py
+++ Wao/settings.py
@@ -172,8 +172,12 @@ if DEB:
 	STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 else:
+	MEDIA_URL = '/media/'
+
+	MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 	STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-	DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+	AWS_PUBLIC_MEDIA_LOCATION = 'media'
+	DEFAULT_FILE_STORAGE = 'Wao.storage_backends.PublicMediaStorage'
 
 	#S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 	#STATIC_URL = 'http://' + S3_BUCKET_NAME + '.s3.amazonaws.com/'
@@ -192,7 +196,7 @@ else:
     'CacheControl': 'max-age=86400',
 }
 	AWS_LOCATION = 'static'
-	
+
 
 	AWS_PRELOAD_METADATA = True
 	STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
--- Wao/urls.py
+++ Wao/urls.py
@@ -64,3 +64,4 @@ urlpatterns = [
 
 
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
