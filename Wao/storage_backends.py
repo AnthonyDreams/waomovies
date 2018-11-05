33f29c7d55065ae730dc150d79b1de770cebe495
Anthony Taveras <theanthony2d@gmail.com>
2018-11-04 22:57:41 -0400

    setting up amazon cloundfront

--- Wao/storage_backends.py
+++ Wao/storage_backends.py
@@ -1,7 +1,12 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
+class StaticStorage(S3Boto3Storage):
 
+    location = settings.AWS_LOCATION
+    def __init__(self, *args, **kwargs):
+    	kwargs['custom_domain'] = settings.AWS_CLOUDFRONT_DOMAIN
+    	super(StaticStorage, self).__init__(*args, **kwargs)
 
 class PublicMediaStorage(S3Boto3Storage):
     location = settings.AWS_PUBLIC_MEDIA_LOCATION
